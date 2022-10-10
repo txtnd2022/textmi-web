f55ee92ca9f7c99b004d8939605375c1e25c636d
txtnd2022 <textmi.global@gmail.com>
2022-10-11 01:25:56 +0530

    11-10-2022

--- components/Home/Sub_Comp/Footer.jsx
+++ components/Home/Sub_Comp/Footer.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Footer() {
     return (
         <>
             <div className="footer">
                 <div className="footer-contents">
-                    <p>Developed by Textmi</p>
-                    <p>
+                    <p>Copyright reserved by Interesting Technologies Private Limited, India.</p>
+                    {/* <p>
                         <> Illustration by < a href="https://icons8.com/illustrations/author/5c07e68d82bcbc0092519bb6" > Icons 8</a > from < a href="https://icons8.com/illustrations" > Ouch!</a > x</>
-                    </p>
+                    </p> */}
+                    <div className='links'>
+                        <p> <Link href={'/termsofuse'} > Terms of Use </Link></p>
+                        <p><Link href={'/privacypolicy'} > Privacy Policy </Link></p>
                 </div>
             </div>
+        </div>
         </>
     )
 }
---
+++ pages/privacypolicy.js
@@ -0,0 +1,221 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Privacypolicy() {
+  return (
+    <div className='conditions'>
+      <h2>Textmi - Privacy Policy</h2>
+      <p>Updated on 12th Oct, 2022</p>
+      <p>If you live outside the European Region, &quot;Textmi&quot; (&quot;Textmi&quot;, &quot;Service&quot;, &quot;we&quot;, &quot;us&quot;,
+        &quot;our&quot;, &quot;Interesting Technologies Private Limited&quot;) provides our services to you
+        under this User Agreement and Privacy Policy. We are pleased to make our
+        Service available to you. Textmi includes the Textmi App, website, features, and
+        other associated services. Textmi provides Instant Messaging, Image Sharing,
+        Video Sharing, Daily Updates Sharing and other services to users. Textmi
+        includes social and interactive features. This is our Privacy Policy that talks about
+        what information we collect and how this affects you. It also explains the steps we
+        take to protect your privacy, like building our Services so delivered messages
+        aren&quot;t stored by us and giving you control over who you communicate with on our
+        Services.
+      </p>
+      <p>If you install, use, access Textmi or any of its features, you have read and
+        accepted the terms and conditions of this Policy. You have also accepted the
+        Textmi &quot;User Agreement&quot;, &quot;Community Guidelines&quot; and other terms which may be
+        available on the Textmi App, Textmi Website, and otherwise. Please make sure to
+        read these documents. By using Textmi, you are entering into a binding
+        contract(s) with Textmi. Your contract with us includes this Policy, the User
+        Agreement, the Community Guidelines, etc. If you don&quot;t agree with and accept the
+        above, please do not use, install, access Textmi or any of its features.</p>
+
+      <p>This Policy is applicable to information provided by users while using Textmi, and
+        not to information made available to Textmi from others. Should Textmi obtain
+        information pertaining to you from websites, apps, landing pages, third parties,
+        etc. that may be accessed by Textmi&quot;s users through Textmi or otherwise, such
+        information will be subject to the privacy and other policies and terms of the said
+        websites, apps, landing pages, etc. This Policy shall not apply to websites, apps,
+        landing pages, etc. or any third-party services accessed by you through Textmi
+        including in case such websites, apps, landing pages, etc. and/or third-party
+        services that are shared on Textmi by other users. </p>
+
+      <p>
+        You also warrant and represent that any registration information and other
+        information that you submit or otherwise provide to Textmi is true, accurate and
+        complete, and you agree to keep it that way at all times.
+      </p>
+
+      <p>If you are subject to EU GDPR, you will be required to provide your explicit
+        consent to the Privacy Policy by clicking on the &quot;I Accept&quot; button. This policy
+        describes:
+      </p>
+      <div className='padded'>
+        <p>● The types of information we may collect or that you may provide when you
+          download, install, register with, access, or use the Textmi application (the
+          <strong>&quot;App&quot;</strong> )</p>
+
+        <p>● Our practices for collecting, using, maintaining, protecting, and disclosing
+          that information.
+        </p>
+      </div>
+
+      <p>This policy applies only to information we collect in this App and in email, text, and
+        other electronic communications sent through or in connection with this App.
+        Please read this policy carefully to understand our policies and practices regarding
+        your information and how we will treat it. If you do not agree with our policies and
+        practices, do not download, register with, or use this App.
+        By downloading, registering with, or using this App, you agree to this privacy
+        policy. This policy may change from time to time. Your continued use of this App
+        after we revise this policy means you accept those changes, so please check the
+        policy periodically for updates.
+      </p>
+
+      <h3>Information We Collect and How We Collect It </h3>
+
+      <p> Information we collect:</p>
+      <p> ● User&quots; phone number.</p>
+      <p> ● Country of origin.</p>
+      <p> ● Name.</p>
+      <p> ● Profile picture.</p>
+
+      <p> We collect information from and about users of our App: </p>
+      <p> ● Directly from you when you provide it to us.</p>
+      <p> ● Automatically when you use the App.</p>
+
+      <h3> Device Services Access and Why We Need It</h3>
+
+      <p>Access permissions that we need:</p>
+      <p>● Contacts: To check for your contacts that have registered on Textmi, so
+        you can message them directly or through a common group on Textmi.</p>
+      <p>● Files: To allow you to send multimedia and files of different formats to your
+        contacts, groups or on your status on Textmi.</p>
+      <p>● Microphone: To record and send voice messages through chats whenever
+        the user wants.</p>
+      <p>● Camera: To access camera when video calling other users on Textmi.</p>
+
+      <h3>Information You Provide to Us</h3>
+      <p>When you download, register with, or use this App, we may ask you to provide information by which you may be personally identified, such as name, email address, telephone number, profile photo, nationality, payment information or any other identifier by which you may be contacted online or offline (&quot;Personal Information&quot;). </p>
+
+      <p>This information includes: </p>
+      <p>● Information provided at the time of registering to use the App, subscribing
+        to our service, and posting material, and requesting further services.</p>
+
+      <p>● Records and copies of your correspondence(including email addresses and phone numbers) if you contact us. </p>
+
+
+      <p> You may also provide information for publication or display (&quot;Posted&quot;) on areas of
+        the app (collectively, &quot;User Contributions&quot;). Your User Contributions are Posted
+        and transmitted to others at your own risk. Additionally, we cannot control the
+        actions of third parties with whom you may choose to share your User
+        Contributions. Therefore, we cannot and do not guarantee that your User
+        Contributions will not be viewed by unauthorized persons. </p>
+
+      <p>The data you share through messages sent to other users are sent through
+        Google Firebase servers because for this beta version as of 21
+        st September, 2022, Textmi(Beta) is using Google Firebase for its server and database needs.
+
+      </p >
+
+      <h3>How We Use Your Information</h3>
+      <p>We use information that we collect about you or that you provide to us, including
+        any personal information, to:</p>
+      <div className='padded'>
+        <p>● Provide you with the App and its contents, and any other information,
+          products, or services that you request from us.</p>
+        <p>● Fulfill any other purpose for which you provide it.</p>
+        <p>● Give you notices about your accoun.</p>
+        <p>● Carry out our obligations and enforce our rights arising from any contracts
+          entered into between you and us.</p>
+      </div>
+
+
+      <h3>Disclosure of Your Information</h3>
+      <p>We may disclose aggregated information about our users, and information that
+        does not identify any individual or device, without restriction.
+        In addition, we may disclose stored information that we collect or you provide:</p>
+      <div className='padded'>
+        <p>● To our subsidiaries and affiliates.</p>
+        <p>● To contractors, service providers, and other third parties we use to support
+          our business and who are bound by contractual obligations to keep
+          personal information confidential and use it only for the purposes for which
+          we disclose it to them.</p>
+        <p>● To a buyer or other successor in the event of a merger, divestiture,
+          restructuring, reorganization, dissolution, or other sale or transfer of some
+          or all of Interesting Technologies Private Limited&quot;s assets, whether as a
+          going concern or as part of bankruptcy, liquidation, or similar proceeding, in
+          which personal information held by Interesting Technologies Private
+          Limited about our App users is among the assets transferred.</p>
+        <p>● To fulfill the purpose for which you provide it.</p>
+        <p>● For any other purpose disclosed by us when you provide the information.</p>
+        <p>● With your consent.</p>
+        <p>● To comply with any court order, law, or legal process, including to respond
+          to any government or regulatory request.</p>
+        <p>● To enforce our rights arising from any contracts entered into between you
+          and us.</p>
+        <p>● If we believe disclosure is necessary or appropriate to protect the rights,
+          property, or safety of Interesting Technologies Private Limited, our
+          customers or others. This includes exchanging information with other
+          companies and organizations for the purposes of fraud protection and
+          credit risk reduction.</p>
+      </div>
+
+      <h3>Accessing and Correcting Your Personal Information</h3>
+      <p>You can review and change your personal information by logging into the App and
+        visiting your account profile page or settings page.</p>
+      <p>We cannot delete your personal information except by also deleting your user
+        account. We may not accommodate a request to change information if we believe
+        the change would violate any law or legal requirement or cause the information to
+        be incorrect</p>
+      <p>If you delete your User Contributions from the App, copies of your User
+        Contributions may remain viewable in cached and archived pages or might have
+        been copied or stored by other App users. Proper access and use of information
+        provided on the App, including User Contributions, is governed by our terms of
+        use</p>
+
+      <h3>Data Security</h3>
+      <p>We have implemented measures designed to secure your personal information
+        from accidental loss and from unauthorized access, use, alteration, and
+        disclosure. All information you provide to us is stored on our secure servers
+        behind firewalls. Any personal information will be encrypted using SSL technology.
+        The safety and security of your information also depends on you. Where we have
+        given you (or where you have chosen) a password for access to certain parts of
+        our App, you are responsible for keeping this password confidential. We ask you
+        not to share your password with anyone. We urge you to be careful about giving
+        out information in public areas of the App. The information you share in public
+        areas may be viewed by other users of the App. Unfortunately, the transmission of information via the internet and mobile platforms
+        is not completely secure. Although we do our best to protect your personal
+        information, we cannot guarantee the security of your personal information
+        transmitted through our App. Any transmission of personal information is at your
+        own risk. We are not responsible for circumvention of any privacy settings or
+        security measures we provide.
+      </p>
+
+      <h3>Changes to Our Privacy Policy</h3>
+      <p>
+        We may update our privacy policy from time to time. If we make material changes
+        to how we treat our users&quot; personal information, we will post the new privacy
+        policy on this page with a notice that the privacy policy has been updated.
+        The date the privacy policy was last revised is identified at the top of the page.
+        You are responsible for ensuring we have up-to-date active and deliverable
+        contact information for you and for periodically visiting this privacy policy to check
+        for any changes.
+      </p>
+
+      <h3>Children Under the Age of 13</h3>
+      <p>
+        The App is not intended for children under 13 years of age, and we do not
+        knowingly collect personal information from children under 13. If we learn we have
+        collected or received personal information from a child under 13 without
+        verification of parental consent, we will delete that information. If you believe we
+        might have any information from or about a child under 13, please contact us at:
+        support@textmi.io
+      </p>
+
+      <h3>Contact Information</h3>
+      <p>
+        To ask questions or comment about this privacy policy and our privacy practices,
+        contact us at: support@textmi.io
+      </p>
+    </div >
+  )
+}
+
+export default Privacypolicy
